3b72192e41aefbe282f7318a8febabc44a870a66
Anita Kumari <anitavkapari@gmail.com>
2018-10-04 17:49:12 +0530

    first commit

---
+++ app/src/main/java/com/example/dell/sharedpreferences/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.dell.sharedpreferences;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    SharedPreferences sharedpreferences;
+    TextView name;
+    TextView email;
+    public static final String mypreference = "mypref";
+    public static final String Name = "nameKey";
+    public static final String Email = "emailKey";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        name = (TextView) findViewById(R.id.etName);
+        email = (TextView) findViewById(R.id.etEmail);
+        sharedpreferences = getSharedPreferences(mypreference,
+                Context.MODE_PRIVATE);
+        if (sharedpreferences.contains(Name)) {
+            name.setText(sharedpreferences.getString(Name, ""));
+        }
+        if (sharedpreferences.contains(Email)) {
+            email.setText(sharedpreferences.getString(Email, ""));
+
+        }
+
+    }
+    public void Save(View view) {
+
+        String n = name.getText().toString();
+        String e = email.getText().toString();
+        SharedPreferences.Editor editor = sharedpreferences.edit();
+        editor.putString(Name, n);
+        editor.putString(Email, e);
+        name.setText("");
+        email.setText("");
+        editor.commit();
+        Toast.makeText(this,"Save",Toast.LENGTH_LONG).show();
+
+    }
+    public void clear(View view) {
+        name = (TextView) findViewById(R.id.etName);
+        email = (TextView) findViewById(R.id.etEmail);
+        name.setText("");
+        email.setText("");
+
+    }
+    public void Get(View view) {
+        name = (TextView) findViewById(R.id.etName);
+        email = (TextView) findViewById(R.id.etEmail);
+        sharedpreferences = getSharedPreferences(mypreference,
+                Context.MODE_PRIVATE);
+
+        if (sharedpreferences.contains(Name)) {
+            name.setText(sharedpreferences.getString(Name, ""));
+        }
+        if (sharedpreferences.contains(Email)) {
+            email.setText(sharedpreferences.getString(Email, ""));
+
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+}
+
+
